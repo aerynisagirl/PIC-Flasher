041d7c396386b6e284827c24d5c34210721373b3
Firealarm-freak <mike.stegne.host@gmail.com>
2016-01-08 11:32:11 -0500

    Ported to a new MCU
    Ported the code to a cheaper MCU that has a lot less features as the
    most of features weren't being used.

--- PIC Flasher.X/Main.c
+++ PIC Flasher.X/Main.c
@@ -2,8 +2,8 @@
  *  PIC Flasher - A pointless flashy blinky thingy which has no purpose but to experiment with delays  *
  *  Created by mikemadealarms on January 7, 2016 at 4:57 PM                                            *
  * --------------------------------------------------------------------------------------------------- *
- *  Last modified by mikemadealarms on January 7, 2016 at 5:56 PM                                      *
- *  Last modification made was: Finish up code and run final test                                      *
+ *  Last modified by mikemadealarms on January 8, 2016 at 11:30 AM                                     *
+ *  Last modification made was: Ported to a cheaper MCU                                                *
  *******************************************************************************************************/
 
 #include <xc.h>
@@ -13,23 +13,16 @@
  ***********************/
 
 //CONFIG1 Register
-#pragma config FOSC = INTOSC   //Configure the MCU to use the internal oscillator
-#pragma config WDTE = OFF      //Disable the Watchdog Timer as it is not needed
-#pragma config PWRTE = OFF     //Disable the Power-up Timer as it is not needed
-#pragma config MCLRE = ON      //Enable the use of the MCLRE function on Pin 4 of the MCU
-#pragma config CP = OFF        //Disable Code Protection as it is completely unecessary
-#pragma config CPD = OFF       //Disable Data Code Protection as it is completely unecessary
-#pragma config BOREN = ON      //Enable the Brown-out Reset function
-#pragma config CLKOUTEN = OFF  //Disable the CLKOUT function on Pin 3 of the MCU
-#pragma config IESO = ON       //Allow the use of Internal/External Clock Switch Over feature
-#pragma config FCMEN = ON      //Enable the Fail-Safe Clock monitor
-
-//CONFIG2 Register
+#pragma config FOSC = INTOSC  //Configure the MCU to use the internal oscillator
+#pragma config BOREN = ON     //Enable the Brown-out Reset function
+#pragma config WDTE = OFF     //Disable the Watchdog Timer as it is not needed
+#pragma config PWRTE = OFF    //Disable the Power-up Timer as it is not needed
+#pragma config MCLRE = ON     //Enable the use of the MCLRE function on Pin 4 of the MCU
+#pragma config CP = OFF       //Disable Code Protection as it is completely unecessary
+#pragma config LVP = OFF      //Disable the Low Voltage Program function linked to the Pin that doesn't exist on the MCU
+#pragma config LPBOR = ON     //Enable the Brown-out Reset function
+#pragma config BORV = LO     //Set the voltage at which the Brown-out Reset function resets the MCU at the low trip point
 #pragma config WRT = OFF     //Disable the Flash-memory Re-Write Protection feature as it is completely unecessary
-#pragma config PLLEN = ON    //Enable the 4xPLL function on the internal oscillator to so the oscillator runs at 32MHz instead of 8MHz
-#pragma config STVREN = OFF  //Disable the Stack Overflow Reset function as it is not needed
-#pragma config BORV = LO     //Set the voltage at which the Brown-out Reset function resets the MCU at to (I don't know as of right now, im assuming its the low voltage setting)
-#pragma config LVP = OFF     //Disable the Low Voltage Program function linked to the Pin that doesn't exist on the MCU
 
 /***************************
  *  Flasher Configuration  *
@@ -55,10 +48,10 @@ int flasherIncrement;       //The increment value that determines how big the sp
 //Setup Function, configures MCU modules, initializes variables, and sets IO registers
 void setup() {
     //Timing Related Registers
-    OSCCON = 0x70;  //Turn on the internal oscillator and set it to run at 8MHz with PLL enabled making it 32MHz
+    OSCCON = 0x70;      //Turn on the internal oscillator and set it to run at 16MHz
     
     //IO Related Registers
-    TRISA = 0x0F;  //Set PORTA4 and PORTA5 to outputs and set the rest to inputs
+    TRISA = 0x0E;   //Set PORTA0 to an output
     
     //Initialize Variables
     flasherSpeed = DELAY_MINIMUM;        //Set the speed of the flasher to begin at the shortest delay the flasher can run at set in the configuration settings
@@ -72,7 +65,7 @@ void setup() {
 //Pause Function, pauses the program for a specified amount of time in milliseconds
 void pause(unsigned long delay) {
     //Loop for the amount of clock cycles required to achieve the specified delay
-    for (unsigned long cycles = 0; cycles < 32000000 / (8000000 / delay); cycles++);
+    for (unsigned long cycles = 0; cycles < 16000000 / (1000000 / delay); cycles++);
 }
 
 //Main Function, called upon reset of the MCU
@@ -82,9 +75,9 @@ void main() {
     //Run in a continuous loop to prevent the MCU from going into sleep mode
     while (1) {
         //Flash the output of the flasher
-        PORTA = 0x30;         //Set PORTA4 and PORTA5 to logic HIGH
+        PORTA = 0x01;         //Set PORTA0 to logic HIGH
         pause(flasherSpeed);  //Wait for the current delay of the flasher and then move on
-        PORTA = 0x00;         //Set PORTA4 and PORTA5 to logic LOW
+        PORTA = 0x00;         //Set PORTA0 to logic LOW
         pause(flasherSpeed);  //Wait for the current delay of the flasher and then move on
         
         //Update the speed of the flasher
--- PIC Flasher.X/nbproject/Makefile-default.mk
+++ PIC Flasher.X/nbproject/Makefile-default.mk
@@ -77,7 +77,7 @@ ifneq ($(INFORMATION_MESSAGE), )
 endif
 	${MAKE}  -f nbproject/Makefile-default.mk dist/${CND_CONF}/${IMAGE_TYPE}/PIC_Flasher.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
 
-MP_PROCESSOR_OPTION=12F1840
+MP_PROCESSOR_OPTION=10F320
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
@@ -111,7 +111,7 @@ endif
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
 dist/${CND_CONF}/${IMAGE_TYPE}/PIC_Flasher.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk    
 	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_CC} $(MP_EXTRA_LD_PRE) --chip=$(MP_PROCESSOR_OPTION) -G -mdist/${CND_CONF}/${IMAGE_TYPE}/PIC_Flasher.X.${IMAGE_TYPE}.map  -D__DEBUG=1 --debugger=pickit3  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"      --ram=default,-160-16f --memorysummary dist/${CND_CONF}/${IMAGE_TYPE}/memoryfile.xml -odist/${CND_CONF}/${IMAGE_TYPE}/PIC_Flasher.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}     
+	${MP_CC} $(MP_EXTRA_LD_PRE) --chip=$(MP_PROCESSOR_OPTION) -G -mdist/${CND_CONF}/${IMAGE_TYPE}/PIC_Flasher.X.${IMAGE_TYPE}.map  -D__DEBUG=1 --debugger=pickit3  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"       --memorysummary dist/${CND_CONF}/${IMAGE_TYPE}/memoryfile.xml -odist/${CND_CONF}/${IMAGE_TYPE}/PIC_Flasher.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}     
 	@${RM} dist/${CND_CONF}/${IMAGE_TYPE}/PIC_Flasher.X.${IMAGE_TYPE}.hex 
 	
 else
